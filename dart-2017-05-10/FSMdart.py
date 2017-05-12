768abfc90a2395b1ab6852ff7f60cb9119ddcb21
CedricDly <cedric.delaunay@ensta-bretagne.org>
2017-05-12 16:24:06 +0200

    fichier FSM

---
+++ dart-2017-05-10/FSMdart.py
@@ -0,0 +1,57 @@
+#coding:utf-8
+
+# FSM du robot DART
+
+#importations
+from dart import Dart
+from threading import Thread
+
+class FSMfacile(Dart):
+    """FSM basique du robot, où les avancées sont pré-programmées, et les rotations aussi (90°)"""
+    
+    def __init__(self):
+        super(FSM,self).__init__()
+        self.state = "arret"
+        self.nxtState = "arret"
+        
+    def arret(self):
+       self.set_speed(0,0)
+   
+    def avancer(self):
+        self.goLineHeading(self.get_angles(),50,5)
+        
+    def rotationDroite(self):
+        self.set_speed(50,-50)
+        
+    def rotationGauche(self):
+        self.set_speed(-50,50)
+    
+    
+    def run(self):        
+        if self.nxtState == "avancer":
+            self.state = "avancer"
+            self.avancer()
+            
+
+        elif self.nxtState == "gauche":
+            self.state = "gauche"
+            self.rotationGauche()
+            
+        elif self.nxtState == "droite":
+            self.state == "droite"
+            self.rotationDroite()
+            
+        elif self.nxtState == "arret":
+            self.state = "arret"
+            self.arret()
+            
+            
+class Key_listener(Thread):
+	def __init__(self, fsm):
+		Thread.__init__(self)
+		self.fsm = FSM()
+	
+	def run(self):
+		while True:
+			l = raw_input("fsm>")
+			self.fsm.nxtState = l
